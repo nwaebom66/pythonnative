9a71ba22a621cca04b7d3cd4f49a282305e129d3
Owen Carey <owenthcarey@gmail.com>
2023-05-27 15:35:12 -0700

    Add Chaquopy to Android

--- apps/android_pythonnative/app/src/main/java/com/pythonnative/pythonnative/MainActivity.kt
+++ apps/android_pythonnative/app/src/main/java/com/pythonnative/pythonnative/MainActivity.kt
@@ -12,22 +12,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pythonnative.pythonnative.ui.theme.PythonnativeTheme
 
+import android.graphics.BitmapFactory
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.chaquo.python.PyException
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PythonnativeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -42,5 +71,71 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     PythonnativeTheme {
         Greeting("Android")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    var bitmap by remember { mutableStateOf<android.graphics.Bitmap?>(null) }
+    val coroutineScope = rememberCoroutineScope()
+
+    // Initialize Chaquopy
+    if (!Python.isStarted()) {
+        Python.start(AndroidPlatform(context))
+    }
+    val py = Python.getInstance()
+    val plotModule = py.getModule("plot")
+
+    // Variables to keep the user's input
+    var xInput by remember { mutableStateOf("") }
+    var yInput by remember { mutableStateOf("") }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = xInput,
+            onValueChange = { xInput = it },
+            label = { Text("Input X") }
+        )
+        OutlinedTextField(
+            value = yInput,
+            onValueChange = { yInput = it },
+            label = { Text("Input Y") }
+        )
+        Button(onClick = {
+            coroutineScope.launch {
+                try {
+                    val bytes = plotModule.callAttr(
+                        "plot",
+                        xInput,
+                        yInput
+                    ).toJava(ByteArray::class.java)
+                    withContext(Dispatchers.IO) {
+                        bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    }
+                } catch (e: PyException) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+                }
+                keyboardController?.hide()
+            }
+        }) {
+            Text(text = "Generate Plot")
+        }
+        bitmap?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = "Generated plot",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                contentScale = ContentScale.FillWidth
+            )
+        }
     }
 }
