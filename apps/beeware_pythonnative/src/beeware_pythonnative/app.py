4845d18020ab2f7608755a28e46f22f1b0db567e
Owen Carey <owenthcarey@gmail.com>
2023-05-30 16:16:19 -0700

    Update BeeWare app

--- apps/beeware_pythonnative/src/beeware_pythonnative/app.py
+++ apps/beeware_pythonnative/src/beeware_pythonnative/app.py
@@ -3,11 +3,10 @@ My first application
 """
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import CENTER, COLUMN, ROW
 
 
 class HelloWorld(toga.App):
-
     def startup(self):
         """
         Construct and show the Toga application.
@@ -16,11 +15,48 @@ class HelloWorld(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        self.main_window = toga.MainWindow(title=self.name)
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.webview = toga.WebView(
+            on_webview_load=self.on_webview_loaded, style=Pack(flex=1)
+        )
+        self.url_input = toga.TextInput(
+            value="https://beeware.org/", style=Pack(flex=1)
+        )
+
+        main_box = toga.Box(
+            children=[
+                toga.Box(
+                    children=[
+                        self.url_input,
+                        toga.Button(
+                            "Go",
+                            on_press=self.load_page,
+                            style=Pack(width=50, padding_left=5),
+                        ),
+                    ],
+                    style=Pack(
+                        direction=ROW,
+                        alignment=CENTER,
+                        padding=5,
+                    ),
+                ),
+                self.webview,
+            ],
+            style=Pack(direction=COLUMN),
+        )
+
         self.main_window.content = main_box
+        self.webview.url = self.url_input.value
+
+        # Show the main window
         self.main_window.show()
+
+    def load_page(self, widget):
+        self.webview.url = self.url_input.value
+
+    def on_webview_loaded(self, widget):
+        self.url_input.value = self.webview.url
 
 
 def main():
