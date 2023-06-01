c4f9c36438541dd13f62cb39d80ba0f4e3f9a2da
Owen Carey <owenthcarey@gmail.com>
2023-06-01 11:28:57 -0700

    Update metadata generator

--- libs/metadata_generators/ios_metadata_generator.py
+++ libs/metadata_generators/ios_metadata_generator.py
@@ -1,14 +1,12 @@
 import clang.cindex
 import json
 
-clang.cindex.Config.set_library_path(
-    "/Applications/Xcode.app/Contents/Developer/Toolchains/"
-    "XcodeDefault.xctoolchain/usr/lib/"
-)
+clang.cindex.Config.set_library_path("/usr/local/Cellar/llvm/16.0.4/lib/")
 index = clang.cindex.Index.create()
 
 
 def extract(cursor):
+    print(f"{cursor.kind}: {cursor.spelling}")
     data = {"Name": cursor.spelling}
     if cursor.kind == clang.cindex.CursorKind.OBJC_INTERFACE_DECL:
         data["Type"] = "Interface"
@@ -29,11 +27,31 @@ def extract(cursor):
 
 
 def main():
+    args = [
+        "-x", "objective-c",
+        "-arch", "arm64",
+        "-fno-objc-arc",
+        # "-fmodules",
+        # "-fmodule-maps",
+        "-ferror-limit=0",
+        "-isysroot",
+        "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk",
+        "-I/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include",
+        "-I/usr/local/include",
+        "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/14.0.3/include",
+        "-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include",
+        "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include",
+        "-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"
+    ]
     tu = index.parse(
         "/Applications/Xcode.app/Contents/Developer/Platforms/"
         "iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/"
-        "Library/Frameworks/UIKit.framework/Headers/UIKit.h"
+        "Library/Frameworks/UIKit.framework/Headers/UIKit.h",
+        args
     )
+    print(f"Translation unit: {tu.spelling}")
+    for diag in tu.diagnostics:
+        print(diag)
     metadata = extract(tu.cursor)
     with open("metadata.json", "w") as f:
         json.dump(metadata, f, indent=2)
