cc4221752a2879bad42dc32fe553d5997ae1ffa4
owenthcarey <owenthcarey@gmail.com>
2023-06-13 14:51:05 -0700

    Add more UIKit widgets

--- apps/ios_pythonnative/create_widgets.py
+++ apps/ios_pythonnative/create_widgets.py
@@ -4,21 +4,80 @@ from rubicon.objc import ObjCClass, ObjCInstance
 UIView = ObjCClass('UIView')
 UIButton = ObjCClass('UIButton')
 UILabel = ObjCClass('UILabel')
+UITextField = ObjCClass('UITextField')
+UISwitch = ObjCClass('UISwitch')
+UISlider = ObjCClass('UISlider')
+UIProgressView = ObjCClass('UIProgressView')
+UIStackView = ObjCClass('UIStackView')
 UIApplication = ObjCClass('UIApplication')
+UIColor = ObjCClass('UIColor')
+NSURL = ObjCClass('NSURL')
+NSData = ObjCClass('NSData')
+UIImage = ObjCClass('UIImage')
+UIImageView = ObjCClass('UIImageView')
+UIActivityIndicatorView = ObjCClass('UIActivityIndicatorView')
 
 
 def create_widgets(view_tag):
     app = UIApplication.sharedApplication
     window = app.keyWindow  # Get key window
     root_view = window.viewWithTag_(view_tag)
-    
+
+    # Create UIStackView
+    stack_view = UIStackView.new().autorelease()
+    stack_view.frame = ((0.0, 0.0), (root_view.frame.size.width,
+                                      root_view.frame.size.height * 0.5))  # X, Y, Width, Height
+    stack_view.axis = 1  # 1 for vertical orientation
+    stack_view.distribution = 0  # 0 for fillEqually
+    stack_view.alignment = 0  # 0 for fill
+    stack_view.spacing = 10.0
+    root_view.addSubview_(stack_view)
+
+    # Create UIImageView
+    url = NSURL.URLWithString_("https://source.unsplash.com/450x300/?nature&random=1")
+    data = NSData.dataWithContentsOfURL_(url)
+    image = UIImage.imageWithData_(data)
+    image_view = UIImageView.alloc().initWithImage_(image).autorelease()
+    stack_view.addArrangedSubview_(image_view)
+
     # Create UILabel
     label = UILabel.new().autorelease()
-    label.frame = ((80.0, 80.0), (160.0, 30.0)) # X, Y, Width, Height
     label.text = "Label created in Python"
-    label.sizeToFit()
-    root_view.addSubview_(label)
+    stack_view.addArrangedSubview_(label)
 
-    # Continue adding other widgets to root_view as needed...
+    # Create UIButton
+    button = UIButton.buttonWithType_(
+        0).autorelease()  # 0 represents UIButtonTypeCustom
+    button.setTitle_forState_("Button created in Python",
+                              0)  # 0 represents normal state
+    button.backgroundColor = UIColor.greenColor  # Set the background color to green
+    button.setTitleColor_forState_(UIColor.blackColor,
+                                   0)  # Set the title color to black for normal state
+    stack_view.addArrangedSubview_(button)
+
+    # Create UITextField
+    text_field = UITextField.new().autorelease()
+    text_field.placeholder = "TextField created in Python"
+    stack_view.addArrangedSubview_(text_field)
+
+    # Create UISwitch
+    switch = UISwitch.new().autorelease()
+    stack_view.addArrangedSubview_(switch)
+
+    # Create UISlider
+    slider = UISlider.new().autorelease()
+    stack_view.addArrangedSubview_(slider)
+
+    # Create UIProgressView
+    progress_view = UIProgressView.new().autorelease()
+    progress_view.progress = 0.66
+    stack_view.addArrangedSubview_(progress_view)
+
+    # Create UIActivityIndicatorView
+    activity_indicator = UIActivityIndicatorView.new().autorelease()
+    activity_indicator.startAnimating()  # Start the animation
+    stack_view.addArrangedSubview_(activity_indicator)  # Add to stack view
+
+    # Continue adding other widgets to stack_view as needed...
 
     return
---
+++ libs/ui_elements/ios_ui_elements.py
@@ -0,0 +1,132 @@
+from rubicon.objc import ObjCClass
+
+# App and Window
+UIApplication = ObjCClass("UIApplication")
+UIWindow = ObjCClass("UIWindow")
+
+# Base classes
+UIView = ObjCClass("UIView")
+UIViewController = ObjCClass("UIViewController")
+
+# Bar classes
+UIBarButtonItem = ObjCClass("UIBarButtonItem")
+UINavigationBar = ObjCClass("UINavigationBar")
+UITabBar = ObjCClass("UITabBar")
+UITabBarItem = ObjCClass("UITabBarItem")
+UIToolbar = ObjCClass("UIToolbar")
+
+# Controls
+UIButton = ObjCClass("UIButton")
+UIDatePicker = ObjCClass("UIDatePicker")
+UIImageView = ObjCClass("UIImageView")
+UILabel = ObjCClass("UILabel")
+UIPickerView = ObjCClass("UIPickerView")
+UIProgressView = ObjCClass("UIProgressView")
+UISegmentedControl = ObjCClass("UISegmentedControl")
+UISlider = ObjCClass("UISlider")
+UISwitch = ObjCClass("UISwitch")
+UITextField = ObjCClass("UITextField")
+UITextView = ObjCClass("UITextView")
+
+# Controllers
+UINavigationController = ObjCClass("UINavigationController")
+UITabBarController = ObjCClass("UITabBarController")
+
+# Indicators
+UIActivityIndicatorView = ObjCClass("UIActivityIndicatorView")
+UIPageControl = ObjCClass("UIPageControl")
+UIProgressView = ObjCClass("UIProgressView")
+UIRefreshControl = ObjCClass("UIRefreshControl")
+
+# Layouts and Views
+UICollectionView = ObjCClass("UICollectionView")
+UIScrollView = ObjCClass("UIScrollView")
+UITableView = ObjCClass("UITableView")
+UIStackView = ObjCClass("UIStackView")
+UIView = ObjCClass("UIView")
+
+# Misc
+UIAlertController = ObjCClass("UIAlertController")
+UIAlertAction = ObjCClass("UIAlertAction")
+UIColor = ObjCClass("UIColor")
+
+# Web view
+WKWebView = ObjCClass("WKWebView")
+
+# Gesture Recognizers
+UIGestureRecognizer = ObjCClass("UIGestureRecognizer")
+UILongPressGestureRecognizer = ObjCClass("UILongPressGestureRecognizer")
+UIPanGestureRecognizer = ObjCClass("UIPanGestureRecognizer")
+UIPinchGestureRecognizer = ObjCClass("UIPinchGestureRecognizer")
+UIRotationGestureRecognizer = ObjCClass("UIRotationGestureRecognizer")
+UIScreenEdgePanGestureRecognizer = ObjCClass("UIScreenEdgePanGestureRecognizer")
+UISwipeGestureRecognizer = ObjCClass("UISwipeGestureRecognizer")
+UITapGestureRecognizer = ObjCClass("UITapGestureRecognizer")
+
+# Audio and Video
+AVAudioPlayer = ObjCClass("AVAudioPlayer")
+AVPlayer = ObjCClass("AVPlayer")
+AVPlayerViewController = ObjCClass("AVPlayerViewController")
+MPMoviePlayerController = ObjCClass("MPMoviePlayerController")
+
+# Data and Documents
+UIDocument = ObjCClass("UIDocument")
+UIDocumentInteractionController = ObjCClass("UIDocumentInteractionController")
+UIDocumentMenuViewController = ObjCClass("UIDocumentMenuViewController")
+UIDocumentPickerViewController = ObjCClass("UIDocumentPickerViewController")
+UIDocumentBrowserViewController = ObjCClass("UIDocumentBrowserViewController")
+
+# Drawing and Graphics
+UIGraphicsImageRenderer = ObjCClass("UIGraphicsImageRenderer")
+
+# Feedback
+UIFeedbackGenerator = ObjCClass("UIFeedbackGenerator")
+
+# Interactions and Effects
+UIBlurEffect = ObjCClass("UIBlurEffect")
+UIInterpolatingMotionEffect = ObjCClass("UIInterpolatingMotionEffect")
+UIVibrancyEffect = ObjCClass("UIVibrancyEffect")
+UIVisualEffect = ObjCClass("UIVisualEffect")
+UIVisualEffectView = ObjCClass("UIVisualEffectView")
+
+# Popovers and Modality
+UIPopoverController = ObjCClass("UIPopoverController")
+UIPresentationController = ObjCClass("UIPresentationController")
+
+# Printing
+UIPrintInteractionController = ObjCClass("UIPrintInteractionController")
+UIPrintPageRenderer = ObjCClass("UIPrintPageRenderer")
+UIPrintPaper = ObjCClass("UIPrintPaper")
+
+# Text and Fonts
+UIFont = ObjCClass("UIFont")
+UIFontDescriptor = ObjCClass("UIFontDescriptor")
+
+# Touches and Gestures
+UITouch = ObjCClass("UITouch")
+
+# Traits and Environment
+UITraitCollection = ObjCClass("UITraitCollection")
+
+# View Controller Presentation
+UIAdaptivePresentationControllerDelegate = ObjCClass(
+    "UIAdaptivePresentationControllerDelegate"
+)
+
+# View Management
+UILayoutGuide = ObjCClass("UILayoutGuide")
+UIResponder = ObjCClass("UIResponder")
+UIScreen = ObjCClass("UIScreen")
+UIViewPropertyAnimator = ObjCClass("UIViewPropertyAnimator")
+UIWindowScene = ObjCClass("UIWindowScene")
+
+# Visualization
+UIBezierPath = ObjCClass("UIBezierPath")
+UIGraphicsRenderer = ObjCClass("UIGraphicsRenderer")
+UIGraphicsRendererContext = ObjCClass("UIGraphicsRendererContext")
+UIGraphicsRendererFormat = ObjCClass("UIGraphicsRendererFormat")
+UIImage = ObjCClass("UIImage")
+UIMotionEffect = ObjCClass("UIMotionEffect")
+
+# Miscellaneous
+UILocalNotification = ObjCClass("UILocalNotification")
