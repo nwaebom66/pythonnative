9ff5d320fc003d217fd9f40d809252a621aa2ce0
owenthcarey <owenthcarey@gmail.com>
2023-06-13 09:35:19 -0700

    Add Python to ios_pythonnative

--- apps/ios_pythonnative/ios_pythonnative/ViewController.swift
+++ apps/ios_pythonnative/ios_pythonnative/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import Python
+import PythonKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let stdLibPath = Bundle.main.path(forResource: "python-stdlib", ofType: nil) else {
+            print("Could not find path to python-stdlib")
+            return
+        }
+        guard let libDynloadPath = Bundle.main.path(forResource: "python-stdlib/lib-dynload", ofType: nil) else {
+            print("Could not find path to python-stdlib/lib-dynload")
+            return
+        }
+                
+        setenv("PYTHONHOME", stdLibPath, 1)
+        setenv("PYTHONPATH", "\(stdLibPath):\(libDynloadPath)", 1)
+                
+        Py_Initialize()
+                
+        let sys = Python.import("sys")
+        print("Python Version: \(sys.version_info.major).\(sys.version_info.minor)")
+        print("Python Encoding: \(sys.getdefaultencoding().upper())")
+        print("Python Path: \(sys.path)")
+                
+        _ = Python.import("math")
     }
 
 
