553e65b2b42c04116366904f1e694e84326b0e0d
owenthcarey <owenthcarey@gmail.com>
2023-06-17 19:45:50 -0700

    Update pythonnative.py

--- libs/pythonnative/pythonnative/pythonnative.py
+++ libs/pythonnative/pythonnative/pythonnative.py
@@ -1,7 +1,7 @@
 # Detect the platform
 import platform
 
-system = platform.system()
+system: str = platform.system()
 
 
 class PlatformNotDetectedError(Exception):
@@ -16,27 +16,27 @@ if system == "iOS":
     class Button:
         native_class = ObjCClass("UIButton")
 
-        def __init__(self, title=""):
+        def __init__(self, title: str = "") -> None:
             self.native_instance = self.native_class.alloc().init()
             self.set_title(title)
 
-        def set_title(self, title):
+        def set_title(self, title: str) -> None:
             self.native_instance.setTitle_forState_(title, 0)
 
-        def get_title(self):
+        def get_title(self) -> str:
             return self.native_instance.titleForState_(0)
 
     class Label:
         native_class = ObjCClass("UILabel")
 
-        def __init__(self, text=""):
+        def __init__(self, text: str = "") -> None:
             self.native_instance = self.native_class.alloc().init()
             self.set_text(text)
 
-        def set_text(self, text):
+        def set_text(self, text: str) -> None:
             self.native_instance.setText_(text)
 
-        def get_text(self):
+        def get_text(self) -> str:
             return self.native_instance.text()
 
 elif system == "Android":
@@ -46,27 +46,27 @@ elif system == "Android":
     class Button:
         native_class = jclass("android.widget.Button")
 
-        def __init__(self, title=""):
+        def __init__(self, title: str = "") -> None:
             self.native_instance = self.native_class()
             self.set_title(title)
 
-        def set_title(self, title):
+        def set_title(self, title: str) -> None:
             self.native_instance.setText(title)
 
-        def get_title(self):
+        def get_title(self) -> str:
             return self.native_instance.getText().toString()
 
     class Label:
         native_class = jclass("android.widget.TextView")
 
-        def __init__(self, text=""):
+        def __init__(self, text: str = "") -> None:
             self.native_instance = self.native_class()
             self.set_text(text)
 
-        def set_text(self, text):
+        def set_text(self, text: str) -> None:
             self.native_instance.setText(text)
 
-        def get_text(self):
+        def get_text(self) -> str:
             return self.native_instance.getText().toString()
 
 else:
