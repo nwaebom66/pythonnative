0bf6938229052e243a2f177433ab16bb7ed0df49
owenthcarey <owenthcarey@gmail.com>
2023-06-19 12:14:04 -0700

    Add template zip files

---
+++ apps/templates/zip_android.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Create a zip file of the Android template in the current directory
+zip -r android_template.zip android_template/
---
+++ apps/templates/zip_ios.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Create a zip file of the iOS template in the current directory
+zip -r ios_template.zip ios_template/
