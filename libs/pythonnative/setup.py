47cc4bc49954a95dd7e1b0f4b7fbb72a36e679ab
owenthcarey <owenthcarey@gmail.com>
2023-06-18 18:44:01 -0700

    Update PyPI version

--- libs/pythonnative/setup.py
+++ libs/pythonnative/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonnative",
-    version="0.0.1",
+    version="0.1.0",
     author="Owen Carey",
     author_email="pythonnative@gmail.com",
     description="A cross-platform Python tool kit for Android and iOS",
