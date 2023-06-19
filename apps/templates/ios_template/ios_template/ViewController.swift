6b5217ff406ecd130e86c734329a459b39984ede
owenthcarey <owenthcarey@gmail.com>
2023-06-19 11:01:59 -0700

    Add iOS template

---
+++ apps/templates/ios_template/ios_template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_template
+//
+//  Created by Owen Carey on 6/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
