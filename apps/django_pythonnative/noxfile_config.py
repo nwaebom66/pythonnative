ff8658a3c8388a7b58a4596b16bb5efb24099249
owenthcarey <owenthcarey@gmail.com>
2023-07-19 17:48:45 -0700

    Update Dockerfile and noxfile_config.py

--- apps/django_pythonnative/noxfile_config.py
+++ apps/django_pythonnative/noxfile_config.py
@@ -20,5 +20,5 @@ TEST_CONFIG_OVERRIDE = {
     # 'gcloud_project_env': 'BUILD_SPECIFIC_GCLOUD_PROJECT',
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
-    "envs": {"DJANGO_SETTINGS_MODULE": "mysite.settings"},
+    "envs": {"DJANGO_SETTINGS_MODULE": "django_pythonnative.settings"},
 }
